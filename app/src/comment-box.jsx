062116b8c659ed3261bfb18cceec81ca5d05015f
Kado Ambrogi <ricardo.ambrogi@gmail.com>
2016-01-21 14:39:20 -0200

    tutorial 12

--- app/src/comment-box.jsx
+++ app/src/comment-box.jsx
@@ -3,12 +3,17 @@ import CommentList from './component/comment-list';
 import CommentForm from './component/comment-form';
 
 class CommentBox extends React.Component{
+	
+	constructor (props) {
+	    super(props)
+	    this.state = { data: [] }
+	}
 
 	render(){
 		return(
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.props.data} />
+				<CommentList data={this.state.data} />
 				<CommentForm />
 			</div>
 		);
