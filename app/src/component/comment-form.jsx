40195db19c2f7dd8c74fa3f64ccc8a1488644956
Kado Ambrogi <ricardo.ambrogi@gmail.com>
2016-01-21 15:49:03 -0200

    tutorial 16

--- app/src/component/comment-form.jsx
+++ app/src/component/comment-form.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
-const CommentForm = () => {
-	return(
-		<form className="commentForm">
-	        <input type="text" placeholder="Your name" />
-	        <input type="text" placeholder="Say something..." />
-	        <input type="submit" value="Post" />
-	    </form>
-	);
+class CommentForm extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = { author: '', text: '' }
+	}
+
+	handleAuthorChange(e){
+		this.setState({author: e.target.value});
+	};
+
+	handleTextChange(e){
+		this.setState({text: e.target.value});
+	}
+
+	render(){
+		return(
+			<form className="commentForm">
+		        <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange}/>
+		        <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange}/>
+		        <input type="submit" value="Post" />
+		    </form>
+		);
+	}
 }
 
 export default CommentForm;
