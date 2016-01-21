de317c0d79d52a68b6bf7f7d6f484c9ee770c0f7
Kado Ambrogi <ricardo.ambrogi@gmail.com>
2016-01-21 14:07:17 -0200

    tutorial 8

--- app/src/component/comment-list.jsx
+++ app/src/component/comment-list.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import Comment from './comment';
 
 const CommentList = (props) => {
+
+	const data = [
+	  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+	  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+	];
+	
 	return(
 		<div className="commentList">
 			<Comment author="Pete Hunt">This is one comment</Comment>
